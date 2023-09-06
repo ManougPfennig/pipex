3d1ac4b4288bd584c4e455fa372a447b00e8f1fb
mapfenni <manoug.pfen@gmail.com>
2023-09-06 15:18:14 +0200

    new parsing

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mapfenni <mapfenni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 18:28:23 by mapfenni          #+#    #+#             */
-/*   Updated: 2023/09/06 14:22:06 by mapfenni         ###   ########.fr       */
+/*   Updated: 2023/09/06 15:16:49 by mapfenni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,20 @@
 
 void	check_open(char **av)
 {
+	int	fd;
+
 	if (access(av[1], F_OK) != 0)
-		exit_msg("Cannot find input file -> ", av[1]);
+		exit_msg("Input file does not exist -> ", av[1]);
 	if (access(av[4], F_OK) != 0)
-		exit_msg("Cannot find output file -> ", av[4]);
+		exit_msg("Input file does not exist -> ", av[4]);
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		exit_msg("Cannot open input file -> ", av[1]);
+	close(fd);
+	fd = open(av[4], O_RDONLY);
+	if (fd == -1)
+		exit_msg("Cannot open output file -> ", av[4]);
+	close(fd);
 }
 
 void	parsing(int ac, char **av)
